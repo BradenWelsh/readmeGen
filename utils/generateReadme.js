9a4141e56c7ce44c1a0a1c27eab3fbc3ee1a46fd
Braden Welsh <BradenWelsh@users.noreply.github.com>
2021-04-11 18:15:23 -0500

    changed GenerateReadme.js

--- utils/generateReadme.js
+++ utils/generateReadme.js
@@ -1,34 +1,34 @@
 function generateReadme(answers){
     const { projectsTitle, projectDesc, install, usage, license, contributors , githubUser, email} = answers
     return `
-![Github License]${license}-blue
+    
 # ${projectsTitle}
 
 #### Table of of Contents
-    1. [Project Description](#project-description)
-    2. [Installation](#install)
-    3. [Usage] (#usage)
-    4. [Contribution Guide] (#contributions)
-    5. [License](#license)
+1. [Project Description](#project-description)
+2. [Installation](#install)
+3. [Usage] (#usage)
+4. [Contribution Guide] (#contributions)
+5. [License](#license)
 
-## Project Description
-    *🔍 ${projectDesc}
+## Project Description 🔍
+* ${projectDesc}
 
-## Installation
-    *💾 ${install}
+## Installation 💾
+* ${install}
 
-## Usage Info
-    *💻 ${usage}
+## Usage Info 💻
+* ${usage}
 
-## Contributors 
-    *👪 ${contributors}
+## Contributors 👪
+* ${contributors}
 
 ## License
-    * Licensed under ![Github License]${license}-blue
+    * Licensed under ${license}
 
-## Questions
-    *✋ Find me on Github @ [${githubUser}](http://github.com/${githubUser})
-    * ✉️For questions about this project please contact me @: ${email}.
+## Questions ✋
+* ✋ Find me on Github @ [${githubUser}](http://github.com/${githubUser})
+* ✉️For questions about this project please contact me @: ${email}.
     
     `;
 }
